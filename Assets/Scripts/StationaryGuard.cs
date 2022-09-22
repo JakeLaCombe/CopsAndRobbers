9e48a5438b52fb71634932f7870055fd1d9f2a2a
Jake LaCombe <jake.lacombe2@gmail.com>
2022-09-22 17:20:54 -0500

    Add in Custom Animations

--- Assets/Scripts/StationaryGuard.cs
+++ Assets/Scripts/StationaryGuard.cs
@@ -8,6 +8,8 @@ public class StationaryGuard : MonoBehaviour, IPlayerActionable
     public AssistantDirections initialDirection = AssistantDirections.LEFT;
     private GameObject GuardRadar;
 
+    private Animator animator;
+
     private Vector2[] radarPoints = {
         new Vector2(-1.0f, 0.0f),
         new Vector2(1.0f, 0.0f),
@@ -25,6 +27,13 @@ public class StationaryGuard : MonoBehaviour, IPlayerActionable
 
         GuardRadar.transform.localPosition = new Vector3(radarPoints[(int)initialDirection].x, radarPoints[(int)initialDirection].y, 0.0f);
         GuardRadar.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, radarRotations[(int)initialDirection]);
+
+        animator = GetComponent<Animator>();
+
+        if (initialDirection == AssistantDirections.LEFT)
+        {
+            this.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
     }
     // Update is called once per frame
     void Update()
@@ -39,21 +48,35 @@ public class StationaryGuard : MonoBehaviour, IPlayerActionable
         if (initialDirection == AssistantDirections.LEFT)
         {
             initialDirection = AssistantDirections.UP;
+
+            animator.SetBool("isFacingUp", true);
+            animator.SetBool("isFacingDown", false);
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
         else if (initialDirection == AssistantDirections.UP)
         {
             initialDirection = AssistantDirections.RIGHT;
+
+            animator.SetBool("isFacingUp", false);
+            animator.SetBool("isFacingDown", false);
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
         else if (initialDirection == AssistantDirections.RIGHT)
         {
             initialDirection = AssistantDirections.DOWN;
+
+            animator.SetBool("isFacingUp", false);
+            animator.SetBool("isFacingDown", true);
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
         else if (initialDirection == AssistantDirections.DOWN)
         {
             initialDirection = AssistantDirections.LEFT;
-        }
 
-        Debug.Log(initialDirection);
+            animator.SetBool("isFacingUp", false);
+            animator.SetBool("isFacingDown", false);
+            this.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
 
         GuardRadar.transform.localPosition = new Vector3(radarPoints[(int)initialDirection].x, radarPoints[(int)initialDirection].y, 0.0f);
         GuardRadar.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, radarRotations[(int)initialDirection]);
